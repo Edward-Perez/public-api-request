78f4f4be9d417b37ee510d2c555d3ee28af50752
Edward Perez <edward.121489@gmail.com>
2019-05-11 10:50:16 +0200

    search bar is functional, refactor

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 const randomUserAPI = 'https://randomuser.me/api/?results=12&nat=us,nz,au';
 const gallery = document.getElementById('gallery');
 const userData = [];
-const headerInner = document.getElementsByClassName('header-inner-container');
-const headerText = document.getElementsByClassName('header-text-container');
 
 
 function initiate(results) {
+    searchBar();
+
     results.forEach(person => {
         userData.push(person);
         gallery.innerHTML += person.card;
     });
-    searchBar();
+
+    gallery.addEventListener('click', (e) => {
+        let jsDataId = e.target.attributes[1].value;
+        if(!isNaN(jsDataId)) {
+            appendModal(jsDataId);
+        }
+    })
 }
 
 function appendModal(jsDataId) {
@@ -43,38 +49,40 @@ function appendModal(jsDataId) {
 }
 
 function searchBar() {
-    const search = document.querySelector('.search-container');
-    let searchString = '';
-    search.innerHTML = `
+    const searchContainer = document.querySelector('.search-container');
+    searchContainer.innerHTML = `
     <form action="#" method="get">
         <input type="search" id="search-input" class="search-input" placeholder="Search...">
         <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
     </form>`;
+    const search = searchContainer.firstElementChild.firstElementChild;
 
     search.addEventListener('keyup', (e) => {
-        console.log(e);
         const userKey = e.key.toLowerCase();
+        const searchVal = search.value.toLowerCase();
         const letterRegex = /^[a-z]$/;
-        if(userKey === 'backspace') {
-            searchString = searchString.slice(0, -1);
+
+        if(letterRegex.test(userKey) || userKey == 'backspace') {
+            search.placeholder = '';
+            search.style.border = '1px solid rgba(200, 200, 200, 0.9)';
+            gallery.innerHTML = '';
+            userData.forEach(person => {
+                if(person.name.search(searchVal) != -1 || person.lsName.search(searchVal) != -1 ) {
+                    gallery.innerHTML += person.card;
+                }
+            })
+            
+        } else if (!isNaN(userKey)) {
+            search.style.border = '2px solid rgba(200, 0, 0, 0.9)';
+            search.value = '';
+            search.placeholder = 'Only letters please.';
         }
-        if(letterRegex.test(userKey)) {
-            searchString += userKey;
-            // console.log(userData[0].name);               working on search function
+
+        if(gallery.innerHTML === '') {
+            gallery.innerHTML = '<h3>Sorry there was no match.</h3>';
         }
     })
 }
-
-
-gallery.addEventListener('click', (e) => {
-    let jsDataId = e.target.attributes[1].value;
-    if(!isNaN(jsDataId)) {
-        appendModal(jsDataId);
-    }
-})
-
-
-
 
 fetch(randomUserAPI)
     .then(response => response.json())
